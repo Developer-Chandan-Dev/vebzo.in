4afe0355b32057d2ae888c675fa4cbada6a19cb5
Chandan Dev <chandan.dev.pro@gmail.com>
2025-03-28 12:55:21 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,11 +8,12 @@ const generateTokenAndSetCookie = (userId, userRole, res) => {
   );
   
   res.cookie("apna_store_jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true, // prevent XSS attact / cross-site scripting attacks
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "production",
-  });
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,  // ✅ Prevents JavaScript access for security
+    secure: true, // ✅ Required for HTTPS (deployment)
+    sameSite: "None", // ✅ Required for cross-site cookies
+  });  
+
 };
 
 module.exports = generateTokenAndSetCookie;

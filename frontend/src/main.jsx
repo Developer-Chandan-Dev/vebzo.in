ff1d0897d5205474fe225fdff6c4f1c163705998
Chandan Dev <chandan.dev.pro@gmail.com>
2025-06-19 11:14:15 +0530

    Update main.jsx Router Added here from App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,15 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./store/store.js";
 import FavsContextProvider from "./context/FavsContext.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <FavsContextProvider>
-        <App />
+        <Router>
+          <App />
+        </Router>
       </FavsContextProvider>
     </Provider>
   </StrictMode>
